547e3521338c561ed2afad76a897b94c5e080bb3
MickyCompanie <micky.companie@gmail.com>
2026-05-15 11:54:19 +0200

    route guard must_be_authenticated

--- backend/app/auth/depedencies.py
+++ backend/app/auth/depedencies.py
@@ -7,7 +7,7 @@ from app.config import Config
 from app.user import services as user_service 
 from app.auth.schemas import TokenData
 
-# Indique à FastAPI où se trouve la route de login pour le bouton "Authorize" du Swagger
+# dit à FastAPI la route de login pour le "Authorize" du Swagger
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{Config.PREFIX}{Config.VERSION}/auth/login")
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
@@ -26,3 +26,12 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     if user is None:
         raise credentials_exception
     return user
+
+def must_be_authenticated(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, Config.JWT_SECRET_KEY, algorithms=[Config.JWT_ALGORITHM])
+        if payload.get("sub") is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=401, detail="Could not validate credentials")
+    return True
