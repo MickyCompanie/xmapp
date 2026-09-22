8fd3dff8aa7b533c9a8f3411c1d25371363144a1
Micky Companie <micky.companie@gmail.com>
2026-09-22 11:48:32 +0200

    fronmt - wish no need for async redundance

--- frontend/app/src/services/wish.js
+++ frontend/app/src/services/wish.js
@@ -2,15 +2,15 @@ import { apiFetch } from './api'
 
 
 const wishApi = {
-    getWishes: async () => await apiFetch('/wish'),
+    getWishes: () => apiFetch('/wish'),
 
-    getWishById: async (id) => await apiFetch(`/wish/${id}`),
+    getWishById: (id) => apiFetch(`/wish/${id}`),
 
-    createWish: async (body) => await apiFetch('/wish/', { method: 'POST', body: body}),
+    createWish: (body) => apiFetch('/wish/', { method: 'POST', body}),
 
-    updateWish: async (body, id) => await apiFetch(`/wish/${id}`, { method: 'PUT', body: body}),
+    updateWish: (body, id) => apiFetch(`/wish/${id}`, { method: 'PUT', body}),
 
-    deleteWish: async (id) => await apiFetch(`/wish/${id}`, {method: 'DELETE'})
+    deleteWish: (id) => apiFetch(`/wish/${id}`, {method: 'DELETE'})
 }
 
 export { wishApi }
